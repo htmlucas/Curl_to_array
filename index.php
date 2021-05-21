328ab48bc3702394d3e9d4c7726c5a0c041eb631
htmlucas <lucasmartinsde@gmail.com>
2021-05-21 17:22:55 -0300

    finalizado

--- index.php
+++ index.php
@@ -1,140 +1,136 @@
 <?php
 
-function curl_get_url_informacoes_estatisticas()
-{
 
-}
+$num_paginas = 54;
 
-function curl_get_post($guarda,$produto,$cmb_grupo,$cmb_tipo_alimento)
-{
+for ($i=0; $i <= $num_paginas ; $i++) { 
 
-}
+    $atuald = 1;
 
-function curl_get_paginacao()
-{
-    /* PÁGINAÇÃO DA CONSULTA */
+    $cont_pagina = 1;
 
-    if($DOM->getElementById('block_1')->textContent){ // consultar se existe páginação na consulta
+
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'http://www.tbca.net.br/base-dados/composicao_alimentos.php?pagina='.$cont_pagina.'&atuald='.$atuald,
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => [
+        'guarda' => '',
+        'produto' => '',
+        'cmb_grupo' => '',
+        'cmb_tipo_alimento' => ''
+        ]
+        ]);
+        // Envio e armazenamento da resposta
+        $response = curl_exec($curl);
         
-        $pagination = $DOM->getElementById('block_1')->textContent;
+        // Fecha e limpa recursos
+        curl_close($curl);
+        
+        curl_error($curl);
 
-        $num_paginas = get_qtd_paginas($pagination);
+        //echo $response;
 
+        $DOM = new DOMDocument();
+        $DOM->loadHTML($response);
 
-    }
+        $DOM->saveHTML();
+        
+        $contador = 0;
 
-}
-function get_qtd_paginas($num_paginas){
-    $qtd_paginas = substr($num_paginas,21); //Pegar o número total de páginas na consulta
-    return $qtd_paginas;
-}
-
-$curl = curl_init();
-curl_setopt_array($curl, [
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://www.tbca.net.br/base-dados/composicao_estatistica.php',
-    CURLOPT_POST => 1,
-    CURLOPT_POSTFIELDS => [
-    'guarda' => '',
-    'produto' => '',
-    'cmb_grupo' => '',
-    'cmb_tipo_alimento' => ''
-    ]
-    ]);
-    // Envio e armazenamento da resposta
-    $response = curl_exec($curl);
-    
-    // Fecha e limpa recursos
-    curl_close($curl);
-    
-    curl_error($curl);
-
-    //echo $response;
-
-    $DOM = new DOMDocument();
-    $DOM->loadHTML($response);
-
-    $DOM->saveHTML();
-    
-    $contador = 0;
-
-    $df = array();
-    
-    $tbodys = $DOM->getElementsByTagName('tbody');
-    foreach($tbodys as $tbody)
-    {
-        $rows = $tbody->getElementsByTagName('tr');
-        foreach($rows as $row)
-        {	
-            $linha = array();
-            $cells = $row->getElementsByTagName('td'); 
-            foreach ($cells as $cell) 
-            {
-                $ahref = $cell->getElementsByTagName('a');
-                foreach($ahref as $a)
+        $df = array();
+        
+        $tbodys = $DOM->getElementsByTagName('tbody');
+        foreach($tbodys as $tbody)
+        {
+            $rows = $tbody->getElementsByTagName('tr');
+            foreach($rows as $row)
+            {	
+                $linha = array();
+                $cells = $row->getElementsByTagName('td'); 
+                foreach ($cells as $cell) 
                 {
-                    array_push($linha, $a->textContent);
-                   
-                }
-                
-            }
-            array_push($df, $linha); // COLOCAR OS VALORES DA LINHA EM OUTRO ARRAY PRA FICAR CERTO
-            
-            $cod_alimento = $df[$contador][0]; // PEGAR O CODIGO DO ALIMENTO DE CADA LINHA
-
-            /* BUSCAR A COMPOSIÇÃO*/ 
-
-            $curl_2 = curl_init();
-
-            // Configura
-            curl_setopt_array($curl_2, [
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'http://www.tbca.net.br/base-dados/int_composicao_alimentos.php?cod_produto='.$cod_alimento
-            ]);
-            
-            // Envio e armazenamento da resposta
-            $response2 = curl_exec($curl_2);
-            
-            // Fecha e limpa recursos
-            curl_close($curl_2);
-            
-            //echo $response2;
-            
-                $DOM2 = new DOMDocument();
-                $DOM2->loadHTML($response2);
-            
-                 $DOM2->saveHTML();
-            
-                $df2 = array();
-                
-                $tablebodys = $DOM2->getElementsByTagName('tbody');
-                foreach($tablebodys as $tbodys){
-                    $rowws = $tbodys->getElementsByTagName('tr');
-                    foreach($rowws as $roww)
-                    {		
-                        $linhas = array();
-                        $celulas = $roww->getElementsByTagName('td'); 
-                        foreach ($celulas as $celula) {
-                                array_push($linhas, $celula->textContent);
-                            
-                        }
-                        array_push($df2, $linhas);	
+                    $ahref = $cell->getElementsByTagName('a');
+                    foreach($ahref as $a)
+                    {
+                        array_push($linha, $a->textContent);
+                    
                     }
-                           
-                }   
-                /* FIM BUSCAR A COMPOSIÇÃO*/ 
-
-                //print_r($df);
-                //print_r($df2);
-               //break;
+                    
+                }
+                array_push($df, $linha); // COLOCAR OS VALORES DA LINHA EM OUTRO ARRAY PRA FICAR CERTO
                 
-            $contador ++;	
-        }
-            
-    }
+                $cod_alimento = $df[$contador][0]; // PEGAR O CODIGO DO ALIMENTO DE CADA LINHA
 
+                /* BUSCAR A COMPOSIÇÃO*/ 
+
+                $curl_2 = curl_init();
+
+                // Configura
+                curl_setopt_array($curl_2, [
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => 'http://www.tbca.net.br/base-dados/int_composicao_alimentos.php?cod_produto='.$cod_alimento
+                ]);
+                
+                // Envio e armazenamento da resposta
+                $response2 = curl_exec($curl_2);
+                
+                // Fecha e limpa recursos
+                curl_close($curl_2);
+                
+                //echo $response2;
+                
+                    $DOM2 = new DOMDocument();
+                    $DOM2->loadHTML($response2);
+                
+                    $DOM2->saveHTML();
+                
+                    $df2 = array();
+                    
+                    $tablebodys = $DOM2->getElementsByTagName('tbody');
+                    foreach($tablebodys as $tbodys){
+                        $rowws = $tbodys->getElementsByTagName('tr');
+                        foreach($rowws as $roww)
+                        {		
+                            $linhas = array();
+                            $celulas = $roww->getElementsByTagName('td'); 
+                            foreach ($celulas as $celula) {
+                                    array_push($linhas, $celula->textContent);
+                                
+                            }
+                            array_push($df2, $linhas);	
+                        }
+                            
+                    }   
+                    /* FIM BUSCAR A COMPOSIÇÃO*/ 
+
+10                    print_r($df);
+                    //print_r($df2);
+                
+                    
+                $contador ++;	
+            }
+                
+        }
+    $num_paginas++;
+    if($i /10 = 0){
+        $atuald++;
+    }
+    if($atuald = 6 and $num_paginas = 54){
+        break;
+    }
+}
     //print_r($df);
     
+
+
+
+
+
+
+
+
 /*  INFORMAÇÕES ESTATISTICAS */
 
 $curl = curl_init();
